9bf5b601a23b29847065948f0518ad66346c07e0
octachron <octa@polychoron.fr>
2015-05-04 17:19:00 +0200

    Local registering and unregistering

--- main.ml
+++ main.ml
@@ -77,6 +77,20 @@ let expr = {
     parse = Parser.parse_expression;
   }
 
+(* Expression sequence destructuring *)
+let seq_destruct expr=
+      match expr.pexp_desc with
+      | Pexp_sequence( el, l ) -> Some (el,l)
+      | Pexp_construct( {Loc.txt = Lid.Lident "!ll_[]"; loc }, None) -> None
+      | exp -> let nowhere = Loc.none in  (*warning: we forge an invalid identifiant to indicate the futur end of the expression sequence *) 
+	       Some (expr,
+		     { pexp_desc = Pexp_construct ({Loc.txt = Lid.Lident "!ll_[]"; loc=nowhere }, None);
+		       pexp_loc = nowhere;
+		       pexp_attributes=[]
+		     } )
+	     
+
+	     
 let patt = {
     mk = (fun ~loc patt -> H.Pat.mk ~loc patt);
     tuple = (fun l -> Ppat_tuple l);
@@ -95,18 +109,10 @@ let patt = {
       | _ -> assert false
     );
   }
-	
-let const_expr mapper f = function
-  | { pexp_desc = Pexp_constant const; pexp_loc } as super ->
-     f expr mapper super pexp_loc const
-  | x -> default_mapper.expr mapper x
+	     
 
-let const_patt mapper f = function
-  | { ppat_desc = Ppat_constant const; ppat_loc } as super ->
-     f patt mapper super ppat_loc const
-  | x ->default_mapper.pat mapper x
 	    
-(* Check if s ends with a ".." token *)
+(* Check if a quoted string ends with a ".." token *)
 let term_parse s=
   let none = false, s in
   let rec seek k =
@@ -170,15 +176,15 @@ let subparser_error loc  =
       raise @@ Loc.Error (Location.error ~loc:(reloc loc) "Error: Syntax error")
   | Ill_formed_ast (loc, s) ->
       Location.raise_errorf ~loc:(reloc loc) "Error: broken invariant in parsetree: %s" s
-			      
- 
-	   
-let string listify kind mapper super loc = function
+			     
+let listify kind term constr ref_loc = mk_list (relocalize ref_loc) kind term constr Loc.{ ref_loc with loc_start = ref_loc.loc_end }  
+			    
+let quoted_string kind mapper super loc = function
   | Asttypes.Const_string(s, Some name) ->
      if Env.mem name !env then
        let constr = Env.find name !env in
-       let term, s = term_parse s in 
-	 let seq_expr =
+       let term, s = term_parse s in
+       let seq_expr =
 	   try
 	     kind.parse Lexer.token @@ Lexing.from_string @@ "["^s^"]"	
 	   with
@@ -195,8 +201,6 @@ let string listify kind mapper super loc = function
      else super
   | x -> super
 	   
-	       
-let listify kind term constr ref_loc = mk_list (relocalize ref_loc) kind term constr Loc.{ ref_loc with loc_start = ref_loc.loc_end }  
 
 let extract_cstring = function
   | {pexp_desc = Pexp_constant (Asttypes.Const_string(s,_) ); _ } -> Some s
@@ -208,6 +212,7 @@ let ( **? ) opt1 opt2 = match opt1,opt2 with
   | _ -> None
 
 let ll_register l =
+  let n = List.length l in
   let args = Array.make 3 "" in
   let free = Array.make 3 true in
   let mark i x = args.(i)<- x; free.(i) <- false in
@@ -220,39 +225,76 @@ let ll_register l =
   let nameless_args = List.fold_left fold [] l in
   let rec assign i name =
     if free.(i) then (mark i name; i - 1) else assign (i-1) name in
-  let _ = List.fold_left assign 2 nameless_args in
-  let name, cons , nil = args.(0), args.(1), args.(2) in
-  env := !env |+> (name, {cons;nil})
+  match n with
+  | 3 -> 
+     let _ = List.fold_left assign 2 nameless_args in
+     let name, cons , nil = args.(0), args.(1), args.(2) in
+     env := !env |+> (name, {cons;nil})
+  | _ -> ()
 	   
+let ll_unregister  = function
+  | [ label, name ] -> env:= Env.remove name !env
+  | l -> () 
+
+let (>>?) x f = match x with
+  | None -> ()
+  | Some x -> f x
+
+let rec map_for_all f = function
+  | [] -> Some []
+  | a::q -> match f a with
+	    | None -> None
+	    | Some x -> match map_for_all f q with
+			| Some l -> Some (x::l)
+			| None -> None
+
+let extract_fun = function
+  | "register" -> ll_register
+  | "unregister" -> ll_unregister
+  | _ -> fun l -> ()
+
+let ( |>? ) x f =
+  match x with None -> None | Some x -> Some (f x)				     
+		    
+(* register literal alias *) 		    
 let extension_iter =
+  let extract_name (e1,n1) = extract_cstring n1 |>? fun n -> (e1,n) in
+  let extract_args = map_for_all extract_name in
   function
   | {pstr_desc = Pstr_eval(
       {pexp_desc = Pexp_apply (
 		       { pexp_desc =
-			   Pexp_ident {Loc.txt=Lid.Lident "register"; loc}; _ },
-		       [ l1, e1; l2, e2; l3, e3 ]
+			   Pexp_ident {Loc.txt=Lid.Lident name ; loc}; _ },
+		       args 
 		     ); _ },
-      _); _  } ->
-     begin
-       let ( ~? ) =  extract_cstring in  
-      match ~?e1 **? ~?e2 **? ~?e3 with
-      | Some (e1,(e2,e3)) -> ll_register [l1,e1; l2, e2; l3, e3]
-      | None -> ()
-     end
+      _); _  } -> extract_args args >>? extract_fun name 
   | _ -> ()
+
+let extension_payload super expr =
+  match expr.pexp_desc with
+  | Pexp_extension( {Loc.txt="listlike"; _ }, PStr l ) ->
+     let () = List.iter extension_iter l in
+     None 
+  | _ -> Some ( super () )
 	   
-let structure_item mapper = function
-  | { pstr_desc = Pstr_eval (
-      {pexp_desc= Pexp_extension ({Loc.txt="listlike"; _ }, payload); _ },
-      att
-    ); _ }  as super
-    -> begin match payload with
-	     | PStr l ->
-		let () = List.iter extension_iter l in
-		None 
-	     | _ -> Some super
-     end
-  | item -> Some (default_mapper.structure_item mapper item)
+let structure_item mapper item =
+  let default () = default_mapper.structure_item mapper item in
+  match item.pstr_desc with 
+  | Pstr_eval (expr,_)  -> extension_payload default expr
+  | item -> Some (default () )
+
+let value_binding mapper {pvb_pat; pvb_expr; pvb_attributes; pvb_loc} =
+  let pvb_pat = mapper.pat mapper pvb_pat in
+  let pvb_expr = mapper.expr mapper pvb_expr in
+  let pvb_loc = mapper.location mapper pvb_loc in
+  let pvb_attributes = mapper.attributes mapper pvb_attributes in
+  {
+    pvb_pat;
+    pvb_expr;
+    pvb_loc;
+    pvb_attributes
+  } 
+         		 
 				       
 let structure_map mapper s =
   let cons_opt l item =
@@ -261,15 +303,40 @@ let structure_map mapper s =
     | None -> l in 
  List.rev @@  List.fold_left cons_opt [] s
 
-				       
-let expression_map mapper = const_expr mapper  @@ string listify
-let pattern_map mapper = const_patt mapper @@ string listify
+
+	
+let const_expr mapper f = function
+  | { pexp_desc = Pexp_constant const; pexp_loc } as super ->
+     f expr mapper super pexp_loc const
+  | x -> default_mapper.expr mapper x
+
+let const_patt mapper f = function
+  | { ppat_desc = Ppat_constant const; ppat_loc } as super ->
+     f patt mapper super ppat_loc const
+  | x ->default_mapper.pat mapper x			     
+
+let unit loc = Pexp_construct ( Loc.{txt= Lid.Lident "()";loc}, None) 
+		
+let expression_map mapper exp =
+  match exp.pexp_desc with
+  | Pexp_constant const -> quoted_string expr mapper exp exp.pexp_loc const
+  | Pexp_extension ({Loc.txt="listlike"; _ }, PStr l) ->
+     List.iter extension_iter l;
+     { exp with pexp_desc = unit exp.pexp_loc }
+  | Pexp_sequence (e1,e2) ->
+     let e1' = mapper.expr mapper e1 in
+     { exp with pexp_desc = Pexp_sequence (e1', mapper.expr mapper e2 ) } 
+  | _ -> default_mapper.expr mapper exp
+
+    
+let pattern_map mapper = const_patt mapper @@ quoted_string
 							       
 let listlike_mapper argv = { 
   default_mapper with
     expr = expression_map;
     pat= pattern_map;
     structure = structure_map;
+    value_binding
   }
 
 let () = register "listlike" listlike_mapper
--- tests/test.ml
+++ tests/test.ml
@@ -1,4 +1,7 @@
-[%listlike register  ~nil:"Nil" "n" ~cons:"Cons"];;
+let a  = Array.make 10 3
+let _ = a.(1;2;3);;		    
+
+[%listlike register "n"  ~nil:"Nil" ~cons:"Cons"];;
 
 type z  =Zero_type
 type 'a succ = Succ_type
@@ -11,10 +14,14 @@ let rec iter: type z r. ('a->unit) -> ('a,z->r) t -> unit = fun f -> function
   | {n| a; l.. |n} -> f a; iter f l
   | {n| |n} -> ()
 			       
-let l = [1;2;3;4]
-				  
-let {n| a; b.. |n} = {n| 5 ; (let x = 2 in x); 3; 4 |n}
-let l = {n| 1; b.. |n} 
-			 
+let l = [1;2;3;4];;
+
+let {n| a; b.. |n} =
+  {n| 5 ; (let x = 2 in x); 3; 4 |n}
+
+let l = {n| 1; b.. |n};;
+  
+[%listlike unregister "n"]	 
+
 let () = Format.printf "@["; iter (Format.printf "%d@;") l ; Format.printf"@]\n"
 
