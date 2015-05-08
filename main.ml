82f0fe4f2872fe5ce385c71a55641ff03f874546
octachron <octa@polychoron.fr>
2015-05-08 21:59:14 +0200

    Basic quoteless version for expression

--- main.ml
+++ main.ml
@@ -1,138 +1,21 @@
 open Parsetree
-open Ast_mapper
 module H = Ast_helper       
 module Loc = Location
 module Lid = Longident	       
-module E=Env_mapper
+open Env_mapper
 type 'a loc = 'a Loc.loc
 		 
 
 let ppf = Format.err_formatter
+
+type kind = List | Array | String | Bigarray
 	    
-type t = { cons: string; nil: string}
-
-(* Subtree builder abstracting over pattern/expr subtree *)	   
-type ('desc,'ty) submk = {
-    mk : loc:Loc.t -> 'desc -> 'ty;
-    tuple: 'ty list -> 'desc;
-    construct: Lid.t loc -> 'ty option -> 'desc; 
-    destruct: 'ty -> ('ty * 'ty) option;
-    ghost_loc : 'ty -> Loc.t;
-    mapper: mapper -> 'ty -> 'ty;
-    parse : (Lexing.lexbuf->Parser.token) -> Lexing.lexbuf -> 'ty
-  }
-
-(* Correct locations when reconstructing subtree for list litterals *)
-let relocalize ref_loc loc =
-  let open Loc in
-  let ref = ref_loc.loc_start in
-  let repos rel =
-    Lexing.{
-	ref with
-	pos_lnum = ref.pos_lnum + rel.pos_lnum - 1;
-	pos_cnum = ref.pos_cnum + rel.pos_cnum - 1 (* -1 for the ghost "[" *) 
-    } in
-  { loc_ghost = loc.loc_ghost;
-    loc_start = repos loc.loc_start;
-    loc_end = repos loc.loc_end
-  }
-    
-let mk submk {cons;_} inner_loc loc ~hd ~tail =
-  let args =  submk.mk ~loc @@ submk.tuple [hd; tail] in
-  submk.mk ~loc @@
-    submk.construct Loc.{ txt=Lid.Lident cons; loc=inner_loc } (Some args)
-
-let mk_nil submk {nil;_} nil_loc =
-   let loc = Loc.{ nil_loc with loc_ghost = true } in
-   let nil = Loc.{ txt = Lid.Lident nil; loc } in
-   submk.mk ~loc @@ submk.construct nil None
-	   
-let rec mk_list reloc submk term kind nil_loc mapper expr=
-  match submk.destruct expr with
-  | None -> mk_nil submk kind nil_loc	 		    
-  | Some(e1, el) ->
-     let hd = submk.mapper mapper e1 in
-     match submk.destruct el, term with
-     | Some _, _ | _, false -> 
-      let tail = mk_list reloc submk term kind nil_loc mapper el
-      and loc = reloc @@ submk.ghost_loc hd in
-      mk submk kind loc loc ~hd ~tail
-     | None, true -> hd
-
-let expr = {
-    mk =(fun ~loc expr ->  H.Exp.mk ~loc expr);
-    tuple = (fun l -> Pexp_tuple l);
-    construct = (fun loc args -> Pexp_construct(loc,args) );
-    destruct =(
-      fun expr ->
-      match expr.pexp_desc with
-      | Pexp_construct(
-	  {Loc.txt=Lid.Lident "::" ; loc },
-	  Some {pexp_desc=Pexp_tuple [el;l];_}
-	) -> Some(el, l)
-      | Pexp_construct( {Loc.txt=Lid.Lident "[]";loc}, None) ->
-	 None
-      | _ -> assert false );
-    ghost_loc  = (fun expr -> Loc.{ expr.pexp_loc with loc_ghost=true } ); 
-    mapper = ( fun mapper expr -> mapper.expr mapper expr);
-    parse = Parser.parse_expression;
-  }
-
-(* Expression sequence destructuring *)
-let seq_destruct expr=
-      match expr.pexp_desc with
-      | Pexp_sequence( el, l ) -> Some (el,l)
-      | Pexp_construct( {Loc.txt = Lid.Lident "!ll_[]"; loc }, None) -> None
-      | exp -> let nowhere = Loc.none in  (*warning: we forge an invalid identifiant to indicate the futur end of the expression sequence *) 
-	       Some (expr,
-		     { pexp_desc = Pexp_construct ({Loc.txt = Lid.Lident "!ll_[]"; loc=nowhere }, None);
-		       pexp_loc = nowhere;
-		       pexp_attributes=[]
-		     } )
-	     
-
-	     
-let patt = {
-    mk = (fun ~loc patt -> H.Pat.mk ~loc patt);
-    tuple = (fun l -> Ppat_tuple l);
-    construct = (fun loc args -> Ppat_construct(loc,args) );
-    mapper = (fun mapper patt -> mapper.pat mapper patt); 
-    ghost_loc  = (fun pattern -> Loc.{ pattern.ppat_loc with loc_ghost=true } );
-    parse = Parser.parse_pattern;
-    destruct = (
-      fun pattern ->
-      match pattern.ppat_desc with
-      | Ppat_construct(
-	  {Loc.txt=Lid.Lident "::"; loc },
-	  Some {ppat_desc=Ppat_tuple [el;l];_}
-	) -> Some(el, l)
-      | Ppat_construct( {Loc.txt=Lid.Lident "[]"; loc }, None ) -> None
-      | _ -> assert false
-    );
-  }
-	     
-
-	    
-(* Check if a quoted string ends with a ".." token *)
-let term_parse s=
-  let none = false, s in
-  let rec seek k =
-    if k<1 then none else
-      match s.[k-1],s.[k]  with
-      | _, (' '|'\009'|'\012'|'\010'|'\013')  -> seek (k-1)
-      | '*',')' -> ignore_comments 1 (k-1)
-      | '.','.' -> true, String.sub s 0 (k-1)
-      | _ -> none 
-  and ignore_comments cl k =
-    if cl = 0 then seek k else
-    if k<1 then none else
-      match s.[k-1],s.[k] with
-      | '*',')' -> ignore_comments (cl+1) (k-2)
-      | '(','*' -> ignore_comments (cl-1) (k-2)
-      | _ -> ignore_comments cl (k-1) in
-  seek @@ (String.length s) - 1
-	    
-module Env =
+module Cons = struct	    
+    type t = { kind:kind; cons: string; nil: string}
+    let default = "ll", { kind=List; cons="Cons"; nil="Nil" }
+  end
+		
+module Defs =
   struct 
     include Map.Make(
 		struct
@@ -141,26 +24,40 @@ module Env =
 		end)
     let (|+>) env (key,value) = add key value env 		  
   end
-    
-type array_kind = Array | String | Bigarray
-	  
-module Indices =
-  struct
+
+module Status = struct
     include Map.Make(
 		struct
-		  type t=array_kind
-		  let compare (x:array_kind) y= compare x y
+		  type t=kind
+		  let compare (x:kind) y = compare x y
 		end)
-    let (|+>) env (key,value) = add key value env 		  
-  end
-    
-let env =
-  Env.(
-    empty
-    |+> ( "ll",  { cons="Cons"; nil="Nil" })
-    |> ref
-  )
 
+    type st = Cons.t t 
+    let (|+>) env constr =
+      let open Cons in add constr.kind constr env
+    let find_opt kind status =
+      try Some (find kind status) with
+      | Not_found -> None 
+
+end
+		      
+module Env = struct
+    type t = {
+	defs: Cons.t Defs.t;
+	status: Status.st
+      }
+	       
+	   
+    let empty = { defs = Defs.empty; status=Status.empty }	     
+    let define ~env ~def =
+      { env with defs = Defs.( env.defs |+> def ) }
+    let activate ~env ~cons =
+      { env with status = Status.( env.status |+> cons )}   
+				      
+		       
+    let default = define empty Cons.default 
+  end
+(*
 let array_env =
   Indices.(
     empty
@@ -169,6 +66,7 @@ let array_env =
     |+> (Bigarray, {cons="Cons_bigarray"; nil= "Nil_bigarray"})
   )
 
+	       
 let identify_array_kind lid =
   let open Lid in
   match lid with
@@ -185,181 +83,88 @@ let identify_array_kind lid =
       )  ->
      Some Bigarray
   | _ -> None 
+ *)
 
-       
-(* Imported from Syntaxerr.ml with location information correction *)       
-let subparser_error loc  =
-  let open Syntaxerr in
-  let reloc = relocalize loc in
-  function
-  | Unclosed(opening_loc, opening, closing_loc, closing) ->
-      Location.raise_errorf ~loc:(reloc closing_loc)
-        ~sub:[
-          Location.error ~loc:(reloc opening_loc)
-            (Printf.sprintf "Error: This '%s' might be unmatched" opening)
-        ]
-        ~if_highlight:
-          (Printf.sprintf "Syntax error: '%s' expected, \
-                           the highlighted '%s' might be unmatched"
-             closing opening)
-        "Error: Syntax error: '%s' expected" closing
+module Opt = struct
+    
+    let (>>?) x f = match x with
+      | None -> ()
+      | Some x -> f x
 
-  | Expecting (loc, nonterm) ->
-     Location.raise_errorf ~loc:(reloc loc)
-			   "Error: Syntax error: %s expected." nonterm
-  | Not_expecting (loc, nonterm) ->
-     Location.raise_errorf ~loc:(reloc loc)
-		     "Error: Syntax error: %s not expected." nonterm
-  | Applicative_path loc ->
-      Location.raise_errorf ~loc:(reloc loc)
-        "Error: Syntax error: applicative paths of the form F(X).t \
-         are not supported when the option -no-app-func is set."
-  | Variable_in_scope (loc, var) ->
-      Location.raise_errorf ~loc:(reloc loc)
-        "Error: In this scoped type, variable '%s \
-         is reserved for the local type %s."
-        var var
-  | Other loc ->
-      raise @@ Loc.Error (Location.error ~loc:(reloc loc) "Error: Syntax error")
-  | Ill_formed_ast (loc, s) ->
-      Location.raise_errorf ~loc:(reloc loc) "Error: broken invariant in parsetree: %s" s
-			     
-let listify kind term constr ref_loc = mk_list (relocalize ref_loc) kind term constr Loc.{ ref_loc with loc_start = ref_loc.loc_end }  
-			    
-let quoted_string kind mapper super loc = function
-  | Asttypes.Const_string(s, Some name) ->
-     if Env.mem name !env then
-       let constr = Env.find name !env in
-       let term, s = term_parse s in
-       let seq_expr =
-	   try
-	     kind.parse Lexer.token @@ Lexing.from_string @@ "["^s^"]"	
-	   with
-	   |  Syntaxerr.Error err -> subparser_error loc err
-	   | Parsing.Parse_error ->
-	      raise @@ Loc.Error(
-			   Loc.error ~loc "Error parsing listlike literal"
-			 )
-	 in
-	 let new_mapper =
-	   { mapper with location = (fun mapper loc' ->
-				     relocalize loc loc' ) } in 
-	 listify kind term constr loc new_mapper seq_expr
-     else super
-  | x -> super
-	   
-
-let extract_cstring = function
-  | {pexp_desc = Pexp_constant (Asttypes.Const_string(s,_) ); _ } -> Some s
-  | _ -> None
-
-
-let ll_register l =
-  let n = List.length l in
-  let args = Array.make 3 "" in
-  let free = Array.make 3 true in
-  let mark i x = args.(i)<- x; free.(i) <- false in
-  let fold l = function
-    | "name", x ->  mark 0 x; l 
-    | "cons", x  -> mark 1 x; l
-    | "nil", x -> mark 2 x; l
-    | "", x -> x::l
-    | _ -> assert false in
-  let nameless_args = List.fold_left fold [] l in
-  let rec assign i name =
-    if free.(i) then (mark i name; i - 1) else assign (i-1) name in
-  match n with
-  | 3 -> 
-     let _ = List.fold_left assign 2 nameless_args in
-     let name, cons , nil = args.(0), args.(1), args.(2) in
-     env := Env.( !env |+> (name, {cons;nil}) )
-  | _ -> ()
-	   
-let ll_unregister  = function
-  | [ label, name ] -> env:= Env.remove name !env
-  | l -> () 
-
-let (>>?) x f = match x with
-  | None -> ()
-  | Some x -> f x
-
-let rec map_for_all f = function
-  | [] -> Some []
-  | a::q -> match f a with
-	    | None -> None
-	    | Some x -> match map_for_all f q with
-			| Some l -> Some (x::l)
-			| None -> None
-
-let extract_fun = function
-  | "register" -> ll_register
-  | "unregister" -> ll_unregister
-  | _ -> fun l -> ()
-
-let ( |>? ) x f =
-  match x with None -> None | Some x -> Some (f x)				     
+    let ( >>=? ) x f =
+      match x with None -> None | Some x -> f x
 		    
-(* register literal alias *) 		    
-let extension_iter =
-  let extract_name (e1,n1) = extract_cstring n1 |>? fun n -> (e1,n) in
-  let extract_args = map_for_all extract_name in
-  function
-  | {pstr_desc = Pstr_eval(
-      {pexp_desc = Pexp_apply (
-		       { pexp_desc =
-			   Pexp_ident {Loc.txt=Lid.Lident name ; loc}; _ },
-		       args 
-		     ); _ },
-      _); _  } -> extract_args args >>? extract_fun name 
-  | _ -> ()
+		    
+    let rec map_for_all f = function
+      | [] -> Some []
+      | a::q -> match f a with
+		| None -> None
+		| Some x -> match map_for_all f q with
+			    | Some l -> Some (x::l)
+  			    | None -> None
 
-let extension_payload super expr =
-  match expr.pexp_desc with
-  | Pexp_extension( {Loc.txt="listlike"; _ }, PStr l ) ->
-     let () = List.iter extension_iter l in
-     None 
-  | _ -> Some ( super () )
-	   
-let structure_item mapper item =
-  let default () = default_mapper.structure_item mapper item in
-  match item.pstr_desc with 
-  | Pstr_eval (expr,_)  -> extension_payload default expr
-  | item -> Some (default () )
+    let ( |>? ) x f =
+      match x with None -> None | Some x -> Some (f x)
 
-let value_binding mapper {pvb_pat; pvb_expr; pvb_attributes; pvb_loc} =
-  let pvb_pat = mapper.pat mapper pvb_pat in
-  let pvb_expr = mapper.expr mapper pvb_expr in
-  let pvb_loc = mapper.location mapper pvb_loc in
-  let pvb_attributes = mapper.attributes mapper pvb_attributes in
-  {
-    pvb_pat;
-    pvb_expr;
-    pvb_loc;
-    pvb_attributes
-  } 
+    let may f param x = match param with
+      | Some p -> f p x
+      | None -> x
+
+    let (><?) opt default = match opt with
+      | Some x -> x
+      | None -> default
+		  
+  end
+open Opt		  
          		 
-				       
-let structure_map mapper s =
-  let cons_opt l item =
-    match structure_item mapper item with
-    | Some x -> x::l
-    | None -> l in 
- List.rev @@  List.fold_left cons_opt [] s
+
+let expr_constr cons lid=
+  let open Loc in
+  let open Lid in
+  let open Cons in
+  match lid.txt with
+  | Lident "::" ->  { lid with txt = Lident cons.cons }
+  | Lident "[]" -> { lid with txt = Lident cons.nil }
+  | _ -> lid
 
 
-	
-let const_expr mapper f = function
-  | { pexp_desc = Pexp_constant const; pexp_loc } as super ->
-     f expr mapper super pexp_loc const
-  | x -> default_mapper.expr mapper x
+module Expr = struct	   
+    let ppx_interpreter mapper env expr =
+      rm_env mapper.expr mapper env expr	   
+	     
+	     
+    let extract  = function
+      | PStr [ {pstr_desc = Pstr_eval (expr, _) ; _ } ] -> Some expr
+      | _ -> None
+	       
+    let extension mapper env super (name, payload) =
+      let open Env in
+      let open Loc in
+      match name.txt, extract payload with
+      | "ppx_listlike", Some expr -> ppx_interpreter mapper env expr
+      | s, Some expr -> (
+	try
+	  let cons = Defs.find s env.defs in
+	  rm_env mapper.expr mapper (activate env cons) expr 
+	with Not_found -> super
+      )
+      | _ -> super
+  end
+		
+	 
+let expr mapper env expr =
+  let open Status in
+  let open Env in
+  match expr.pexp_desc with
+  | Pexp_construct (lid, expr_opt) ->
+     let lid =  Opt.may expr_constr (find_opt List env.status) lid
+     and expr_opt = expr_opt |>? rm_env mapper.expr mapper env in
+     env, { expr with pexp_desc = Pexp_construct( lid, expr_opt) }
+  | Pexp_extension ext -> env, Expr.extension mapper env expr ext
+  | _ -> Env_mapper.identity.expr mapper env expr 
 
-let const_patt mapper f = function
-  | { ppat_desc = Ppat_constant const; ppat_loc } as super ->
-     f patt mapper super ppat_loc const
-  | x ->default_mapper.pat mapper x			     
 
-let unit loc = Pexp_construct ( Loc.{txt= Lid.Lident "()";loc}, None)
-		      
+(*		      
 let uniformize_args kind mapper loc  =
   function
   | arg1::(lbl,seq)::q ->
@@ -376,8 +181,8 @@ let uniformize_args kind mapper loc  =
 	 seq
      )::q
   | args -> args
-    
-		
+ *)  
+(*		
 let expression_map mapper exp =
   let default () = default_mapper.expr mapper exp in
   match exp.pexp_desc with
@@ -403,16 +208,13 @@ let expression_map mapper exp =
       | _ -> default ()
     end 
   | _ -> default ()
+ *)
+    			       
+let listlike_mapper argv =
+  to_transform Env.default
+	       { 
+		 identity  with
+		 expr
+	       }
 
-    
-let pattern_map mapper = const_patt mapper @@ quoted_string
-							       
-let listlike_mapper argv = { 
-  default_mapper with
-    expr = expression_map;
-    pat= pattern_map;
-    structure = structure_map;
-    value_binding
-  }
-
-let () = register "listlike" listlike_mapper
+let () = Ppx_register.register "listlike" listlike_mapper
--- tests/test.ml
+++ tests/test.ml
@@ -1,8 +1,8 @@
 let a  = Array.make 10 3
 let _ = a.(1;2;3);;		    
 
-[%listlike register "n"  ~nil:"Nil" ~cons:"Cons"];;
-
+(*[%listlike register "n"  ~nil:"Nil" ~cons:"Cons"];;
+ *)
 type z  =Zero_type
 type 'a succ = Succ_type
 		 
@@ -11,17 +11,11 @@ type ('a,_) t =
   | Nil : ('a, 'base -> 'base ) t
 	    
 let rec iter: type z r. ('a->unit) -> ('a,z->r) t -> unit = fun f -> function
-  | {n| a; l.. |n} -> f a; iter f l
-  | {n| |n} -> ()
+  | Cons(a,l) -> f a; iter f l
+  | Nil -> ()
 			       
-let l = [1;2;3;4];;
+let l =[%ll[1;2;3;4]];;
 
-let {n| a; b.. |n} =
-  {n| 5 ; (let x = 2 in x); 3; 4 |n}
-
-let l = {n| 1; b.. |n};;
   
-[%listlike unregister "n"]	 
-
 let () = Format.printf "@["; iter (Format.printf "%d@;") l ; Format.printf"@]\n"
 
